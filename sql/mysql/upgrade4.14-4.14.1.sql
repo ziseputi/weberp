bec210fe0c1d5bd50ece6a9c387962316050304f
vijay-prevaj <34272427+vijay-prevaj@users.noreply.github.com>
2017-12-08 09:49:08 +0530

    Update upgrade4.14-4.14.1.sql

--- sql/mysql/upgrade4.14-4.14.1.sql
+++ sql/mysql/upgrade4.14-4.14.1.sql
@@ -1,1 +1,8 @@
 UPDATE config SET confvalue='4.14.1' WHERE confname='VersionNumber';
+ALTER TABLE  `salesman` ADD  `address1` VARCHAR( 30 ) NOT NULL default '0';
+ALTER TABLE  `salesman` ADD  `address2` VARCHAR( 30 ) NOT NULL default '0';
+ALTER TABLE  `salesman` ADD  `address3` VARCHAR( 30 ) NOT NULL default '0';
+ALTER TABLE  `salesman` ADD  `address4` VARCHAR( 30 ) NOT NULL default '0';
+ALTER TABLE  `salesman` ADD  `address5` VARCHAR( 30 ) NOT NULL default '0';
+ALTER TABLE `debtorsmaster` ADD `phone_number` bigint(20) NOT NULL default '0';
+ALTER TABLE `debtorsmaster` ADD `email_id` varchar(30) NOT NULL default '0';
